c91cf8b8380742a2fec7468ff9d979adc18df78e
valentin palitsa <palitsavalentin@gmail.com>
2020-02-05 11:57:10 +0200

    list

--- Program.cs
+++ Program.cs
@@ -12,26 +12,52 @@ namespace ConsoleApp5
     class Program
     {
         private static Person[] people = new Person[10000];
+        private static List<Person> listpeople = new List<Person>();
         private static string path = @"C:\\Users\\valik\\source\\repos\\ConsoleApp5\\ConsoleApp5\\name.txt";
-        
+
+
+
+
 
         static void Main(string[] args)
         {
+            Random random = new Random();
             readOnFile(path);
+            Console.WriteLine(listpeople.Count);
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
+
+            int i = 0;
+            foreach (var item in listpeople)
+            {
+                if (item.Name.Equals("Valik")) {
+
+                    Console.WriteLine(i);
+                    break;
+                }
+                i++;
+            }
+
+
+
+            //Console.WriteLine(listpeople[listpeople.Count-1]);
             //sortOnAge();
-            //bublesort(people);
-            //selectSort(people);
-            //Array.Sort(people);
-            shellSort(people, people.Length);
+            //bublesort(listpeople);
+            //selectSort(listpeople);
+            //listpeople.Sort();
+            //shellSort(listpeople, listpeople.Count);
             stopwatch.Stop();
             time(stopwatch);
-            
             Console.ReadLine();
         }
-        public static void shellSort(Person[] arr, int size) {
-            int increment = 8;
+
+
+
+
+
+
+        public static void shellSort(List<Person> arr, int size) {
+            int increment = 50;
             while (increment > 0)
             {
                 for (int i = 0; i < size; i++)
@@ -47,15 +73,25 @@ namespace ConsoleApp5
                 }
                 if (increment > 1) increment = increment / 2;
                 else if (increment == 1) break;
-
             }
         }
+
+
+
+
+
 
         public static void time(Stopwatch stopwatch) {
             TimeSpan timeSpan = stopwatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
         }
+
+
+
+
+
+
         public static void write(Person [] arr) {
 
             foreach (var item in arr)
@@ -64,13 +100,22 @@ namespace ConsoleApp5
             }
         }
 
-        public static void selectSort(Person[] person) {
+
+
+
+
+
+
+
+
+
+        public static void selectSort(List<Person> person) {
             Person temp;
             int Ind_min = 0, i, j;
-            for (i = 0; i < person.Length; i++)
+            for (i = 0; i < person.Count; i++)
             {
                 Ind_min = i;
-                for ( j = 0; j < person.Length; j++)
+                for ( j = 0; j < person.Count; j++)
                 {
                     if (person[i].Age < person[Ind_min].Age) Ind_min = j;
                 }
@@ -80,11 +125,18 @@ namespace ConsoleApp5
             }
         }
 
-        public static void bublesort(Person [] people) {
+
+
+
+
+
+
+
+        public static void bublesort(List<Person> people) {
             Person person = people[0];
-            for (int i = 0; i < people.Length; i++)
+            for (int i = 0; i < people.Count; i++)
             {
-                for (int j = i+1; j < people.Length; j++)
+                for (int j = i+1; j < people.Count; j++)
                 {
                     if(people[i].Age> people[j].Age) {
                         person = people[i];
@@ -94,20 +146,34 @@ namespace ConsoleApp5
                 }
             }
         }
-        public static void sortOnAge() {
 
-            for (int i = 0; i < people.Length; i++)
+
+
+
+
+
+
+        public static void sortOnAge() {
+            for (int i = 0; i < listpeople.Count; i++)
             {
-                Person cur = people[i];
+                Person cur = listpeople[i];
                 int j = i;
-                while (j>0 && (cur.Age <  people[j-1].Age))
+                while (j>0 && (cur.Age <  listpeople[j-1].Age))
                 {
-                    people[j] = people[j - 1];
+                    listpeople[j] = listpeople[j - 1];
                     j--;
                 }
-                people[j] = cur;
+                listpeople[j] = cur;
             }
         }
+
+
+
+
+
+
+
+
         public static void readOnFile(string path) {
             try {
                 using (StreamReader stream = new StreamReader(path))
@@ -128,8 +194,8 @@ namespace ConsoleApp5
         public static void masAdd(string name, string surname , int number , int age, string otchestvo , int index){
             
             Person person = new Person(name, surname, number, age, otchestvo);
-            people[index] = person;
-           
+            listpeople.Add(person);
+            //people[index] = person;
         }
     }
 }
